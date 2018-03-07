2fa04a5eb5dc373dd4278ea82abb151940e67988
shovan777 <raj.shrestha778@gmail.com>
2018-03-07 14:03:05 +0545

    fire

--- weather_app/playground/promise.js
+++ weather_app/playground/promise.js
@@ -10,9 +10,12 @@ var asyncAdd = (a, b) => {
   })
 };
 
-asyncAdd(5, 7).then((res) => {
+asyncAdd(5, '7').then((res) => {
   console.log('Result: ', res);
-}, (errorMessage) => {
+  return asyncAdd(res, 33);
+}).then((result) => {
+  console.log('should be 45', result);
+}).catch((errorMessage) => {
   console.log(errorMessage);
 });
 // var somePromise = new Promise((resolve, reject) => {
