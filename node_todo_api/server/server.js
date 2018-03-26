f19c2f953828fe757a16560a997d28344bc4cd3d
shovan777 <raj.shrestha778@gmail.com>
2018-03-26 14:40:00 +0545

    Add update feature

--- node_todo_api/server/server.js
+++ node_todo_api/server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -74,6 +75,35 @@ app.delete('/todos/:id',(req, res) => {
   }, (e) => {
     res.status(404).send();
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+    // getTime gives the values in milliseconds
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+
 });
 
 app.listen(port, () => {
